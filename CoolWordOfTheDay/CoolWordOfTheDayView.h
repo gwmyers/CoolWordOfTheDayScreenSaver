058db865ddd59e79ac6f683e0830a221a3014daf
Greg Myers <greg.myers@cern.ch>
2020-12-06 20:35:20 +0100

    Initial Commit

---
+++ CoolWordOfTheDay/CoolWordOfTheDayView.h
@@ -0,0 +1,12 @@
+//
+//  CoolWordOfTheDayView.h
+//  CoolWordOfTheDay
+//
+//  Created by Greg Myers on 12/6/20.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface CoolWordOfTheDayView : ScreenSaverView
+
+@end
---
+++ CoolWordOfTheDay/CoolWordOfTheDayView.m
@@ -0,0 +1,51 @@
+//
+//  CoolWordOfTheDayView.m
+//  CoolWordOfTheDay
+//
+//  Created by Greg Myers on 12/6/20.
+//
+
+#import "CoolWordOfTheDayView.h"
+
+@implementation CoolWordOfTheDayView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
